2f19bc83748d152246222d46aaa0c2ba0e2a5a9e
Guillaume Cummings <guillaume_cummings@hotmail.com>
2019-05-06 16:42:48 -0400

    Only remove first u

--- src/api/ctid-ng.js
+++ src/api/ctid-ng.js
@@ -72,8 +72,7 @@ export default (client: ApiRequester, baseUrl: string) => ({
     }
 
     if (contact) {
-      const trick = `${contact.split('@')[0]  }@127.0.0.1:34464;${  contact.split('@')[1].split(';')[1]}`
-      body.location.contact = trick.substr(1, trick.length);
+      body.location.contact = contact.substr(1, contact.length);
     }
 
     return client.post(`${baseUrl}/users/me/relocates`, body, token);
